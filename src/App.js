8113536e8616c450af4c7220ee97d2c39686ddc3
Nanahawaw <folarinyetunde@gmail.com>
2023-11-16 15:56:04 +0100

    added weather component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
-
+import Weather from "./Weather";
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div className="container mx-auto px-4">
+        Weather App
+        <Weather />
+      </div>
+    </div>
+  );
 }
 
 export default App;
