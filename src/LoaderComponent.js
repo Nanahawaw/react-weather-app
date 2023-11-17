f799b29e46f454ed541383c31805214b43a2ea41
Nanahawaw <folarinyetunde@gmail.com>
2023-11-17 21:41:24 +0100

    adjusted loader component

--- src/LoaderComponent.js
+++ src/LoaderComponent.js
@@ -5,10 +5,10 @@ export default function LoaderComponent() {
   return (
     <ColorRing
       visible={true}
-      height="80"
-      width="80"
+      height="800"
+      width="800"
       ariaLabel="blocks-loading"
-      wrapperStyle={{}}
+      wrapperStyle={{ margin: "0 auto" }}
       wrapperClass="blocks-wrapper"
       colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
     />
